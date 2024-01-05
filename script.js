fa5ecc588084444c582131624c6d49de4fde260f
18praise <jesussosarollie1812@gmail.com>
2024-01-04 23:02:28 -0300

    data gathering finished

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
-let currentLocation = "Pehuajo";
+let currentLocationName = "La Plata";
 
-async function getWeather() {
+async function getData(userLocation) {
   const response = await fetch(
-    `http://api.weatherapi.com/v1/current.json?key=ad7fa77616244e869a1110828231412&q=${currentLocation}&aqi=no`,
+    `http://api.weatherapi.com/v1/current.json?key=ad7fa77616244e869a1110828231412&q=${userLocation}&aqi=no`,
     { mode: "cors" }
   );
   const locationData = await response.json();
-  console.log(locationData.current.temp_c);
-  console.log(locationData.location.name);
+  const processedData = processData(locationData);
+  displayData(processedData);
 }
-getWeather();
+
+function processData(locationData) {
+  locationData = {
+    name: locationData.location.name,
+    region: locationData.location.region,
+    tempC: locationData.current.temp_c,
+    feelsLikeC: locationData.current.feelslike_c,
+    feelsLikeF: locationData.current.feelslike_f,
+    tempF: locationData.current.temp_f,
+    chanceOfRain: locationData.current.precip_mm,
+    condition: locationData.current.condition.text,
+  };
+  return locationData;
+}
+
+function newLocation() {
+  let userLocation = "La Plata";
+  getData(userLocation);
+}
+
+function displayData(data) {
+  console.log(data);
+}
+
+newLocation();
